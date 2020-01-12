a9c29dc734c9435ca1f42ea886c646db8a2bc986
zfima <zzfima@gmail.com>
2020-01-12 16:02:30 +0200

    port from config

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express()
 const config = require('config')
 
 let port = config.get('port') || 5000
-app.listen(port, () => console.log('App has been started'))
+app.listen(port, () => console.log(`App has been started on port ${port}`))
